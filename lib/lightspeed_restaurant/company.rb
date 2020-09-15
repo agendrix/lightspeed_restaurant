2824216d809c52063b3d2054cf08b68b352dba03
lmandrile <laurent.mandrile@gmail.com>
2020-09-15 12:27:27 -0400

    Added support for companies

---
+++ lib/lightspeed_restaurant/company.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'lightspeed_restaurant/base'
+require 'lightspeed_restaurant/operations/list'
+
+module LightspeedRestaurant
+  class Company < LightspeedRestaurant::Base
+    extend Operations::List
+
+    def self.resource_name
+      'Company'
+    end
+
+    def self.default_resource_path
+      "/rest/core/#{resource_name.downcase}"
+    end
+  end
+end
